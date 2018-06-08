927e3b68759de02d7d1cfc3c0632244fd7cf8e8f
Colton Grainger <colton.grainger@colorado.edu>
2018-06-08 00:53:53 -0700

    Remove xmobar spawn, too much CPU for C-q xmonad resets.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,8 +18,7 @@ import XMonad.Actions.Submap       -- (19) create keybinding submaps
 import XMonad.Actions.Search hiding (Query, images) 
                                    -- (20) some predefined web searches
 import XMonad.Actions.WithAll      -- (22) do something with all windows on a workspace
-import XMonad.Actions.SpawnOn      -- (22a) start programs on a particular WS
-import XMonad.Actions.TopicSpace   -- (22b) set a "topic" for each workspace
+import XMonad.Actions.WindowGo
 
 import XMonad.Prompt
 import XMonad.Prompt.Man
@@ -30,9 +29,7 @@ import XMonad.Prompt.Shell
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 
---------------------------------------------------------------------------------
 main = do
-  spawn "xmobar"
 
   xmonad $ desktopConfig
     { borderWidth = 2
@@ -52,15 +49,26 @@ main = do
       -- layout
       , ("M-<Esc>", sendMessage (Toggle "Full"))
       -- start programs
-      , ("M-o", spawn "wine start /unix '/opt/oed/swhx.exe'")
-      , ("M-m", spawn "urxvt -e mutt")
-      , ("M-<Delete>", spawn "urxvt -e htop")
+      , ("M-o", runOrRaise "wine start /unix '/opt/oed/swhx.exe'" (className =? "Oxford English Dictionary"))
+      , ("M-m", runOrRaise "mutt" (className =? "mutt"))
+      , ("M-<Delete>", runOrRaise "htop" (className =? "htop"))
+      -- text editors
+      , ("M-e", runOrRaise "emacs" (className =? "Emacs"))
+      , ("M-S-e", prompt "emacs" myXPConfig)
+      , ("M-v", runOrRaise "urxvt -e vim" (className =? "vim"))
+      , ("M-S-v", prompt "urxvt -e vim" myXPConfig)
+      -- browsing 
+      , ("M-w", runOrRaise "firefox" (className =? "Firefox"))
+      , ("M-S-w", selectSearch duckduckgo)
+      , ("M-/", promptSearch myXPConfig duckduckgo)
+      -- edit config
+      , ("M-c e", spawn "emacs ~/.emacs.d/init.el")
+      , ("M-c v", spawn "urxvt -e vim $HOME/.vimrc")
+      , ("M-c m", spawn "urxvt -e vim $HOME/.mutt")
+      , ("M-c x", spawn "urxvt -e vim $HOME/.xmonad/xmonad.hs")
       -- prompts
       , ("M-p", shellPrompt myXPConfig)
       , ("M-h", manPrompt myXPConfig)                           -- (24)
-      -- searches
-      , ("M-/", promptSearch myXPConfig duckduckgo)
-      , ("M-S-/", selectSearch duckduckgo)
       -- lock the screen with xscreensaver
       , ("M-S-l", spawn "xscreensaver-command -lock")             -- (0)
       -- bainsh the pointer
@@ -91,6 +99,6 @@ myManageHook = composeOne
 -- customLogHook to show windows in xmobar
 quamashPP :: PP
 quamashPP = def { ppCurrent = xmobarColor "white" "black"
-                 , ppTitle = xmobarColor "yellow" "" . shorten 60
-                 , ppLayout = const "" -- to disable the layout info on xmobar
-                 }
+                , ppTitle = xmobarColor "yellow" "" . shorten 60
+                , ppLayout = const "" -- to disable the layout info on xmobar
+                }
