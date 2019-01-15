9a5df74974753be00ec1bf8c0473ec31869e4bc6
Colton Grainger <colton.grainger@colorado.edu>
2019-01-14 20:51:01 -0700

    Decide no to Fira Sans

--- .local/bin/scripts/math6220-compile.sh
+++ .local/bin/scripts/math6220-compile.sh
@@ -4,7 +4,6 @@
 # 2019-01-14
 # CC0 Public Domain
 
-pandoc -s --filter pandoc-citeproc -o ${1%.*}.tex $1 --template=amsart.latex -f markdown-auto_identifiers 
-sleep .5
+pandoc -s --filter pandoc-citeproc -o ${1%.*}.tex $1 -f markdown-auto_identifiers 
 latexmk -pdf ${1%.*}.tex
 latexmk -c ${1%.*}.tex
--- .local/bin/scripts/math6220.sh
+++ .local/bin/scripts/math6220.sh
@@ -4,8 +4,7 @@
 # 2019-01-14
 # CC0 Public Domain
 
-pandoc -s --filter pandoc-citeproc -o ${1%.*}.tex $1 --template=amsart.latex -f markdown-auto_identifiers 
-sleep .5
+pandoc -s --filter pandoc-citeproc -o ${1%.*}.tex $1 -f markdown-auto_identifiers 
 latexmk -pdf ${1%.*}.tex
 latexmk -c ${1%.*}.tex
 mupdf ${1%.*}.pdf &
