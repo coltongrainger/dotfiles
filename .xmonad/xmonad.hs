9926b062429a1ea88db21a7290f467d5c1c0c56d
Colton Grainger <colton.grainger@colorado.edu>
2019-07-09 11:27:52 -0600

    Provide multiple xmobars

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,13 @@
 import System.Exit
 import XMonad
 
-import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicBars
 
 import XMonad.Layout.NoBorders (noBorders)
-import XMonad.Layout.ResizableTile (ResizableTall(..))
 import XMonad.Layout.ToggleLayouts (ToggleLayout(..), toggleLayouts)
-import XMonad.Layout.StackTile
 
 import XMonad.Actions.Warp         -- (18) warp the mouse pointer
 import XMonad.Actions.Submap       -- (19) create keybinding submaps
@@ -29,6 +28,8 @@ import XMonad.Prompt.Shell
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
 -- import XMonad.Util.WorkspaceScreenshot
+import XMonad.Util.WorkspaceCompare
+import XMonad.Util.Scratchpad
 
 import qualified XMonad.StackSet as W
 import XMonad.Util.NamedWindows ( getName )
@@ -38,15 +39,17 @@ import Data.List ( (\\), intercalate )
 
 main = do
 
-  xmonad $ desktopConfig
-    { terminal    = "urxvt"
-    , workspaces  = map show [1..9]
-    , borderWidth = 1
-    , modMask     = mod4Mask -- Use the "Win" key for the mod key
-    , manageHook  = myManageHook <+> manageHook desktopConfig
-    , layoutHook  = desktopLayoutModifiers $ myLayouts
-    , logHook     = dynamicLogString quamashPP >>= xmonadPropLog
-    -- , startupHook = myStartupHook
+  xmonad $ def
+    { terminal           = "urxvt"
+    , workspaces         = map show [1..9]
+    , borderWidth        = 2
+    , focusedBorderColor = "#dc322f"
+    , modMask            = mod4Mask -- Use the "Win" key for the mod key
+    , startupHook        = spawn "$HOME/.xmonad/autostart.sh" <+> dynStatusBarStartup barCreate barDestroy
+    , manageHook         = myManageHook
+    , layoutHook         = myLayouts
+    , handleEventHook    = docksEventHook <+> dynStatusBarEventHook barCreate barDestroy
+    , logHook            = dynamicLogString quamashPP >>= xmonadPropLog
     }
 
     `removeKeysP`
@@ -59,6 +62,7 @@ main = do
       , ("M-S-'", banishScreen LowerRight)                          -- (18)
       -- window movements 
       , ("M-<Esc>", sendMessage (Toggle "Full"))
+      , ("M-b", sendMessage ToggleStruts)
       , ("<XF86Forward>" , rotAllDown)
       , ("<XF86Back>" , rotAllUp)
       -- kill windows
@@ -91,13 +95,14 @@ main = do
       , ("M-m", raiseMaybe (runInTerm "-title mutt" "bash -c mutt") (title =? "mutt")) -- (m)utt
       , ("M-n", runOrRaise "mnemosyne" (className =? "mnemosyne")) -- m(n)emosyne
       , ("M-o", raiseMaybe (spawn "wine start /unix '/opt/oed/swhx.exe'") (title =? "Oxford English Dictionary")) -- (o)ed
-      --, ("M-v", raiseMaybe (runInTerm "-title GhostTextStart" "bash -c 'vim -c \"GhostTextStart\"'") (title =? "GhostTextStart"))
       , ("M-z", runOrRaise "zotero" (className =? "Zotero")) -- (z)otero
       , ("M-<Delete>", raiseMaybe (runInTerm "-title htop" "bash -c htop") (title =? "htop"))
       , ("M-<F5>", raiseMaybe (spawn "/opt/cisco/anyconnect/bin/vpnui") (className =? "Cisco AnyConnect Secure Mobility Client"))
+      , ("M-d", raiseMaybe (spawn "/home/colton/.local/dropbox.py start &") (className =? "Cisco AnyConnect Secure Mobility Client"))
       , ("M-\\", runOrRaise "keepassxc" (className =? "keepassxc")) -- M-S-\ runs autotype ... this shortcut is configured in keepassxc itself
       -- config files
       -- vimrc
+      , ("M-v", scratchpadSpawnActionTerminal "urxvt")
       , ("M-c v", raiseMaybe (runInTerm "-title .vimrc" "bash -c 'vim $HOME/.vimrc'") (title =? ".vimrc"))
       -- init.el
       , ("M-c s", raiseMaybe (runInTerm "-title init.el" "bash -c 'vim $HOME/.emacs.d/init.el'") (title =? "init.el"))
@@ -125,21 +130,32 @@ myXPConfig = def { position          = Top
                  , font    = "-uw-*-medium-i-*-*-14-*-*-*-*-*-*-*"
                  , fgColor = "#839496"
                  , bgColor = "#002b36"
+                 , historySize = 512
+                 , showCompletionOnTab = True
+                 , historyFilter = deleteConsecutive
                  }
 
-myLayouts = toggleLayouts (noBorders Full) others
-  where
-    others = ResizableTall 1 (1.5/100) (5/8) [] -- ||| StackTile 1 (3/100) (1/2)
+myLayouts = avoidStruts (tall ||| Mirror tall)
+                   where  tall = Tall 1 (3/100) (1/2)
 
-myManageHook = composeOne
-  [ isDialog              -?> doCenterFloat
-  , transience
-  ]
+myManageHook :: ManageHook
+myManageHook = composeAll
+                [ isFullscreen                  --> doFullFloat
+                , isDialog --> doCenterFloat
+		, className =? "Xmessage" 	--> doCenterFloat 
+		, className =? "Xfce4-notifyd" --> doIgnore
+	    	, className =? "stalonetray" --> doIgnore
+		, manageDocks
+	    	, scratchpadManageHook (W.RationalRect 0.125 0.25 0.75 0.5)
+                ]
 
 -- customLogHook to show windows in xmobar
 quamashPP :: PP
-quamashPP = def { ppCurrent = xmobarColor "#268bd2" ""
-                , ppTitle = xmobarColor "#b58900" "" . shorten 20
+quamashPP = def { ppCurrent = xmobarColor "#dc322f" "" . wrap "[" "]"
+                , ppVisible = xmobarColor "#b58900" "" . wrap "(" ")"
+                -- , ppSort    = getSortByXineramaRule
+                , ppTitle = xmobarColor "#dc322f" "" . shorten 25
+                , ppSep = " + "
                 , ppLayout = const "" -- to disable the layout info on xmobar
                 , ppExtras = [logTitles]
                 , ppOrder  = \(ws:l:t:ts:_) -> ws : t : [xmobarColor "#839496" "" ts]
@@ -147,6 +163,12 @@ quamashPP = def { ppCurrent = xmobarColor "#268bd2" ""
 
 -- https://stackoverflow.com/questions/22838932/
 logTitles :: X (Maybe String) -- this is a Logger
-logTitles = withWindowSet $ fmap (Just . intercalate "  :  ") -- fuse window names
-                          . traverse (fmap (shorten 15) . fmap show . getName) -- show window names
+logTitles = withWindowSet $ fmap (Just . intercalate " + ") -- fuse window names
+                          . traverse (fmap (shorten 25) . fmap show . getName) -- show window names
                           . (\ws -> W.index ws \\ maybeToList (W.peek ws))
+
+-- https://github.com/peterzky/myxmonad/blob/master/xmonad.hs
+barCreate (S 0) = spawnPipe $ "xmobar -x 0"
+barCreate (S sid) = spawnPipe $ "xmobar -x " ++ show sid
+
+barDestroy = return ()
